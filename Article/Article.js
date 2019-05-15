d4bf4d1151e3cdb2fc52a2b6746d172e4e2f7704
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-15 14:23:17 +0100

    Completed the close button stretch goal

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,25 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
 
   expandArticle() {
+    this.domElement.classList.toggle("article-open")
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open')
+   if (this.domElement.classList.contains("article-open")){
+     this.expandButton.textContent = "close"
+   }
+   else {
+     this.expandButton.textContent = "expand"
+   }
   }
+ 
 }
 
 /* START HERE: 
@@ -30,4 +37,4 @@ let articles;
 articles = document.querySelectorAll(".article");
 articles.forEach(element => {
   new Article(element);
-})
+});
